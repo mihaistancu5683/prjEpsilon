69a3cfb8791a9e5628340c34ce3858c8ef54ebdf
mihaistancu5683 <mihaistancu@mail.com>
2017-06-11 14:57:42 +0300

    Replaced IconMenu with AppBar

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -2,13 +2,18 @@ import React, { Component } from 'react';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import RaisedButton from 'material-ui/RaisedButton';
 
-import IconMenuExampleSimple from './IconMenu.jsx';
+// We import all of the components used in App
+import AppBarExampleComposition from './AppBar.jsx';
 
+// App is interiting from the Component class
 export default class App extends Component {
     render() {
         return(
+            //MuiThemeProvider expects only one child element
             <MuiThemeProvider>
-                <IconMenuExampleSimple />
+                <div>
+                    <AppBarExampleComposition />
+                </div>
             </MuiThemeProvider>
         )
     }
---
+++ imports/ui/AppBar.jsx
@@ -0,0 +1,80 @@
+import React, {Component} from 'react';
+import AppBar from 'material-ui/AppBar';
+import IconButton from 'material-ui/IconButton';
+import IconMenu from 'material-ui/IconMenu';
+import MenuItem from 'material-ui/MenuItem';
+import FlatButton from 'material-ui/FlatButton';
+import Toggle from 'material-ui/Toggle';
+import MoreVertIcon from 'material-ui/svg-icons/navigation/more-vert';
+import NavigationClose from 'material-ui/svg-icons/navigation/close';
+
+class Login extends Component {
+  static muiName = 'FlatButton';
+  render() {
+    return (
+      <FlatButton {...this.props} label="Login" />
+    );
+  }
+}
+
+const Logged = (props) => (
+  <IconMenu
+    //{...props}
+    iconButtonElement={
+      <FlatButton label="Welcome" />
+    }
+    targetOrigin={{horizontal: 'right', vertical: 'top'}}
+    anchorOrigin={{horizontal: 'right', vertical: 'top'}}
+  >
+  </IconMenu>
+);
+
+const MenuItems = (props) => (
+  <IconMenu
+    iconButtonElement={
+      <IconButton><MoreVertIcon /></IconButton>
+    }
+    targetOrigin={{horizontal: 'right', vertical: 'top'}}
+    anchorOrigin={{horizontal: 'right', vertical: 'top'}}
+  >
+    <MenuItem primaryText="Home" />
+    <MenuItem primaryText="Option 1" />
+    <MenuItem primaryText="Option 2" />
+    <MenuItem primaryText="Option 3" />
+    <MenuItem primaryText="Option 4" />
+    <MenuItem primaryText="Option 5" />
+  </IconMenu>
+);
+
+Logged.muiName = 'IconMenu';
+
+class AppBarExampleComposition extends Component {
+  state = {
+    logged: true,
+  };
+
+  handleChange = (event, logged) => {
+    this.setState({logged: logged});
+  };
+
+  render() {
+    return (
+      <div>
+        <Toggle
+          label="Logged"
+          defaultToggled={true}
+          onToggle={this.handleChange}
+          labelPosition="right"
+          style={{margin: 20}}
+        />
+        <AppBar
+          title="Menu"
+          iconElementLeft={<MenuItems />}
+          iconElementRight={this.state.logged ? <Logged /> : <Login />}
+        />
+      </div>
+    );
+  }
+}
+
+export default AppBarExampleComposition;
