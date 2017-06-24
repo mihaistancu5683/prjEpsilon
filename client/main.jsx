394bab0f76e58d5a6d9d40b5e140682c25934b55
mihaistancu5683 <mihaistancu@mail.com>
2017-06-24 23:09:54 +0300

    Creates mock "Register" component for writing new user into the database

--- client/main.jsx
+++ client/main.jsx
@@ -2,11 +2,13 @@ import React from 'react';
 import { Meteor } from 'meteor/meteor';
 import { render } from 'react-dom';
 import injectTapEventPlugin from 'react-tap-event-plugin'; // For touches and clicks within React
+import {Router, Route, browserHistory} from 'react-router'; // For routing
 
 import App from '../imports/ui/App.jsx';
+import Register from '../imports/ui/register.jsx';
 
 injectTapEventPlugin();
 
 Meteor.startup(() => {  //will run in the client because we are in the Client folder
-    render(<App />, document.getElementById('render-target')); //render App.jsx in main.html, in render-target div
+    render(<Register />, document.getElementById('render-target')); //render App.jsx in main.html, in render-target div
 });
--- server/main.js
+++ server/main.js
@@ -1,5 +1,8 @@
 import { Meteor } from 'meteor/meteor';
+import Users from '../imports/api/users.js';
 
 Meteor.startup(() => {
-  // code to run on server at startup
+  Meteor.publish('players', function() {
+    return User.find({});
+  });
 });
