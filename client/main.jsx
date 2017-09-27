e8176d44ad1ee4a22e46c873d8822b6bf00290dc
mihaistancu5683 <mihaistancu@mail.com>
2017-09-27 20:23:02 +0300

    Adds eslint and fixes two files

--- client/main.jsx
+++ client/main.jsx
@@ -1,10 +1,8 @@
-import React from 'react';
+/* eslint-env browser */
 import { Meteor } from 'meteor/meteor';
 import { render } from 'react-dom';
-
 // add render routes function
 import { renderRoutes } from '../imports/startup/client/routes';
-
 // pass an anonymous function that will execute as soon as the DOM is ready
 Meteor.startup(() => {
   render(renderRoutes(), document.getElementById('render-target'));
--- imports/startup/client/routes.js
+++ imports/startup/client/routes.js
@@ -1,29 +1,23 @@
 import React from 'react';
-import {   BrowserRouter, Route } from 'react-router-dom'
+import { BrowserRouter, Route } from 'react-router-dom';
 
 // containers
-import MainPage from './../../ui/containers/MainPage';
+import MainPage from './../../ui/containers/MainPage.jsx';
 
 // pages
-import SignupPage from './../../ui/pages/SignupPage';
-import LoginPage from './../../ui/pages/LoginPage';
-import AddSubject from './../../ui/pages/addSubject';
-import Lost from './../../ui/pages/lost';
+import SignupPage from './../../ui/pages/SignupPage.jsx';
+import LoginPage from './../../ui/pages/LoginPage.jsx';
+import AddSubject from './../../ui/pages/addSubject.jsx';
+import Lost from './../../ui/pages/lost.jsx';
 
-/* history={history} */
 export const renderRoutes = () => (
-  <BrowserRouter> 
-      <Main />
+  <BrowserRouter>
+    <div>
+      <Route exact path="/" component={MainPage} />
+      <Route path="/login" component={LoginPage} />
+      <Route path="/signup" component={SignupPage} />
+      <Route path="/add_subject" component={AddSubject} />
+      <Route path="*" component={Lost} />
+    </div>
   </BrowserRouter>
 );
-
-const Main = () => (
-  <main>
-    <div>
-      <Route exact={true} path="/" component={MainPage} />
-      <Route path="/login" component={LoginPage}/>
-      <Route path="/signup" component={SignupPage}/>
-      <Route path='/add_subject' component={AddSubject}/>
-      <Route path='*' component={Lost}/>
-    </div>
-  </main>)
