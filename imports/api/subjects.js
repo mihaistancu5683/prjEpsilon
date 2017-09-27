f03792aab353fdb34541c74748a406a564d07244
mihaistancu5683 <mihaistancu@mail.com>
2017-09-27 20:54:39 +0300

    Eslint

--- imports/api/subjects.js
+++ imports/api/subjects.js
@@ -1,11 +1,15 @@
-import {Mongo} from 'meteor/mongo';
+import { Mongo } from 'meteor/mongo';
+/* eslint import/no-unresolved: 0 */
+import { SimpleSchema } from 'meteor/aldeed:simple-schema';
+
 export const Subjects = new Mongo.Collection('subjects');
+
 const SubjectSchema = new SimpleSchema({
-	subjectId: { type: String, optional: false},
-	subjectTitle: { type: String, optional: false},
-	subjectDescription: { type: String, optional: false },
-	subjectCategory: { type: String, optional: false },
-	subjectStatus: { type: String, optional: false },
-	subjectAuthor: { type: String, optional: false },
+  subjectId: { type: String, optional: false },
+  subjectTitle: { type: String, optional: false },
+  subjectDescription: { type: String, optional: false },
+  subjectCategory: { type: String, optional: false },
+  subjectStatus: { type: String, optional: false },
+  subjectAuthor: { type: String, optional: false },
 });
 Subjects.attachSchema(SubjectSchema);
