d21d0067a2ac25f1cfea247c31da29a01a620a0b
mihaistancu5683 <mihaistancu@mail.com>
2017-09-27 22:22:50 +0300

    Eslint

--- imports/ui/components/SubjectList.jsx
+++ imports/ui/components/SubjectList.jsx
@@ -1,33 +1,19 @@
-import React, {Component} from 'react';
+import { Meteor } from 'meteor/meteor';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import {
   Table,
   TableBody,
-  TableFooter,
   TableHeader,
   TableHeaderColumn,
   TableRow,
   TableRowColumn,
 } from 'material-ui/Table';
-import TextField from 'material-ui/TextField';
-import Toggle from 'material-ui/Toggle';
-import { createContainer } from 'meteor/react-meteor-data'
-import { Link } from 'react-router'; // Link add subject to the route
-import { Subjects } from '../../api/subjects.js';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import { createContainer } from 'meteor/react-meteor-data';
+import { Subjects } from '../../api/subjects.js';
 
-const styles = {
-  propContainer: {
-    width: 200,
-    overflow: 'hidden',
-    margin: '20px auto 0',
-  },
-  propToggleHeader: {
-    margin: '20px auto 10px',
-  },
-};
-
-export  class TableExampleComplex extends Component {
+export class SubjectList extends Component {
   state = {
     selectable: false,
     displaySelectAll: false,
@@ -50,11 +36,11 @@ export  class TableExampleComplex extends Component {
   };
 
   handleChange = (event) => {
-    this.setState({height: event.target.value});
+    this.setState({ height: event.target.value });
   };
 
-  renderSubjects(){
-    return this.props.subjects.map((subject) => (
+  renderSubjects() {
+    return this.props.subjects.map(subject => (
       <TableRow key={subject.subjectId}>
         <TableRowColumn>{subject.subjectId}</TableRowColumn>
         <TableRowColumn>{subject.subjectTitle}</TableRowColumn>
@@ -66,48 +52,49 @@ export  class TableExampleComplex extends Component {
   render() {
     return (
       <MuiThemeProvider>
-      <div>
-        <Table
-          height={this.state.height}
-          fixedHeader={this.state.fixedHeader}
-          fixedFooter={this.state.fixedFooter}
-          selectable={this.state.selectable}
-          multiSelectable={this.state.multiSelectable}
-        >
-          <TableHeader
-            displaySelectAll={this.state.displaySelectAll}
-            adjustForCheckbox={this.state.showCheckboxes}
-            enableSelectAll={this.state.enableSelectAll}
+        <div>
+          <Table
+            height={this.state.height}
+            fixedHeader={this.state.fixedHeader}
+            fixedFooter={this.state.fixedFooter}
+            selectable={this.state.selectable}
+            multiSelectable={this.state.multiSelectable}
           >
-            <TableRow>
-              <TableHeaderColumn tooltip="The code">Code</TableHeaderColumn>
-              <TableHeaderColumn tooltip="The Title">Title</TableHeaderColumn>
-              <TableHeaderColumn tooltip="The Status">Status</TableHeaderColumn>
-            </TableRow>
-          </TableHeader>
-          <TableBody
-            displayRowCheckbox={this.state.showCheckboxes}
-            deselectOnClickaway={this.state.deselectOnClickaway}
-            showRowHover={this.state.showRowHover}
-            stripedRows={this.state.stripedRows}
-          >
-            {this.renderSubjects()}
-          </TableBody>
-        </Table>
-      </div>
+            <TableHeader
+              displaySelectAll={this.state.displaySelectAll}
+              adjustForCheckbox={this.state.showCheckboxes}
+              enableSelectAll={this.state.enableSelectAll}
+            >
+              <TableRow>
+                <TableHeaderColumn tooltip="The code">Code</TableHeaderColumn>
+                <TableHeaderColumn tooltip="The Title">Title</TableHeaderColumn>
+                <TableHeaderColumn tooltip="The Status">Status</TableHeaderColumn>
+              </TableRow>
+            </TableHeader>
+            <TableBody
+              displayRowCheckbox={this.state.showCheckboxes}
+              deselectOnClickaway={this.state.deselectOnClickaway}
+              showRowHover={this.state.showRowHover}
+              stripedRows={this.state.stripedRows}
+            >
+              {this.renderSubjects()}
+            </TableBody>
+          </Table>
+        </div>
       </MuiThemeProvider>
     );
   }
 }
 
-TableExampleComplex.propTypes = {
+SubjectList.propTypes = {
   subjects: PropTypes.array.isRequired,
 };
 
-export default createContainer (() => {
+export default createContainer(() => {
   Meteor.subscribe('subjects');
-  
+
   return {
-    subjects: Subjects.find({}).fetch(), //subjects: Subjects.find({name: "SearchNameHere"}, {sort: {name:1}}).fetch(),
+    // subjects: Subjects.find({name: "SearchNameHere"}, {sort: {name:1}}).fetch(),
+    subjects: Subjects.find({}).fetch(),
   };
-}, TableExampleComplex);
+}, SubjectList);
--- imports/ui/containers/MainPage.jsx
+++ imports/ui/containers/MainPage.jsx
@@ -1,71 +1,72 @@
+import PropTypes from 'prop-types';
+import { Meteor } from 'meteor/meteor';
 import React, { Component } from 'react';
-import { withHistory } from 'react-router-dom';
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import { Link } from 'react-router-dom';
-import { PropTypes } from 'prop-types';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import LoginContainer from './LoginContainer.jsx';
+import Subjects from '../components/SubjectList.jsx';
 
-import LoginContainer from './LoginContainer';
-import TableExampleComplex from '../components/SubjectList.jsx';
-
-export default class AppContainer extends Component {
-  constructor(props){
-    super(props);
-    this.state = this.getMeteorData();
-    this.logout = this.logout.bind(this);
-  }
-
-  getMeteorData(){
+export default class MainPage extends Component {
+  // Functions that do no use "this" should be static
+  static getMeteorData() {
     return { isAuthenticated: Meteor.userId() !== null };
   }
 
-  componentWillMount(){
+  constructor(props) {
+    super(props);
+    this.state = MainPage.getMeteorData();
+    this.logout = this.logout.bind(this);
+  }
+
+  componentWillMount() {
     if (!this.state.isAuthenticated) {
       this.props.history.push('/login');
     }
   }
 
-  componentDidUpdate(prevProps, prevState){
+  componentDidUpdate() {
     if (!this.state.isAuthenticated) {
       this.props.history.push('/login');
     }
   }
 
-  logout(e){
+  logout(e) {
     e.preventDefault();
-    Meteor.logout( (err) => {
-        if (err) {
-            console.log( err.reason );
-        } else {
-            this.props.history.push('/login');
-        }
+    Meteor.logout((err) => {
+      if (err) {
+        console.log(err.reason);
+      } else {
+        this.props.history.push('/login');
+      }
     });
     this.props.history.push('/login');
   }
 
   render() {
-    let currentUser = this.props.currentUser;
-    let userDataAvailable = (currentUser !== undefined);
-    let loggedIn = (currentUser && userDataAvailable);
     return (
       <MuiThemeProvider>
         <div>
           <nav className="nav-extended">
             <div className="nav-wrapper">
-              <a href="#" className="brand-logo">Project Epsilon</a>
+              <a href="" className="brand-logo">Project Epsilon</a>
               <div>
                 <ul id="nav-mobile" className="right hide-on-med-and-down">
-                  <li> <a href="#"><LoginContainer />          </a></li>
-                  <li> <a href="#" onClick={this.logout}>Logout</a> </li>
-                </ul>  
-                {this.props.children}
+                  <li> <a href=""><LoginContainer /></a></li>
+                  <li> <a href="" onClick={this.logout}>Logout</a> </li>
+                </ul>
               </div>
             </div>
           </nav>
-            <div id="nav-wrapper">
-                <TableExampleComplex />
-                <Link to="/add_subject">Add subject </Link>
-            </div>
+          <div id="nav-wrapper">
+            <Subjects />
+            <Link to="/add_subject">Add subject </Link>
+          </div>
         </div>
       </MuiThemeProvider>
     );
-  }}
+  }
+}
+
+MainPage.propTypes = {
+  history: PropTypes.object, // eslint-disable-line react/forbid-prop-types
+};
