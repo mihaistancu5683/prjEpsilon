756a58045feffea6711e2072d80203770e652418
mihaistancu5683 <mihaistancu@mail.com>
2017-10-01 22:33:35 +0300

    Eslint

--- imports/ui/pages/lost.jsx
+++ imports/ui/pages/lost.jsx
@@ -1,16 +1,14 @@
-import React, { Component } from 'react';
+import React from 'react';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
 // App is interiting from the Component class
-export default class Lost extends Component {
-  render() {
-    return (
-    // MuiThemeProvider expects only one child element
-      <MuiThemeProvider>
-        <div>
-          <h1>404</h1>
-        </div>
-      </MuiThemeProvider>
-    );
-  }
-}
+export const Lost = () => (
+  // MuiThemeProvider expects only one child element
+  <MuiThemeProvider>
+    <div>
+      <h1>404</h1>
+    </div>
+  </MuiThemeProvider>
+  );
+
+export default Lost;
